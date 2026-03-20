6934813634fa00d02ea1221810c8c12147706137
Nicholas Hassan <nick@hassan.host>
2026-03-20 09:22:47 +0100

    Run virby-vm directly under launchd

--- module/default.nix
+++ module/default.nix
@@ -85,10 +85,11 @@ let
     }
   );
 
-  runnerScript = pkgs.writeShellScript "${daemonName}-runner" ''
+  prepareVmScript = pkgs.writeShellScript "${daemonName}-prepare-vm" ''
     PATH=${binPath}:$PATH
 
     set -euo pipefail
+    cd ${workingDirectory}
 
     NEEDS_GENERATE_SSH_KEYS=0
 
@@ -319,6 +320,13 @@ in
 
         chown ${darwinUser}:${darwinGroup} ${workingDirectory}
 
+        logInfo "Preparing Virby VM runtime files..."
+        if ! ${prepareVmScript}; then
+          logError "Failed to prepare Virby VM runtime files"
+          exit 1
+        fi
+
+        chown -R ${darwinUser}:${darwinGroup} ${workingDirectory}
       '';
 
       environment.etc."ssh/ssh_config.d/100-${vmHostName}.conf".text = ''
@@ -339,8 +347,17 @@ in
 
       launchd.daemons = {
         ${daemonName} = {
-          path = [ "/bin" ];
-          command = runnerScript;
+          path =
+            with pkgs;
+            [
+              coreutils
+              findutils
+              gnugrep
+              nix
+              openssh
+              self.packages.${pkgs.stdenv.hostPlatform.system}.vm-runner
+            ];
+          command = "${lib.getExe self.packages.${pkgs.stdenv.hostPlatform.system}.vm-runner}";
 
           serviceConfig = {
             UserName = darwinUser;
